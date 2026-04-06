d27764654b0a21c204d0470f61d3e22295f4d8c5
abinashbl7 <abinashbls7@gmail.com>
2026-04-06 17:41:37 +0530

    correctTest

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -19,6 +19,6 @@ describe('App', () => {
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
    expect(compiled.querySelector('h1')?.textContent)
-  .toContain('Angular 20');
+  .toContain('Angular 19');
   });
 });
